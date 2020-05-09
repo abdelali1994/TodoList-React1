e7f4e7ada2f6de97838293d865a3833136d7630a
abdelali1994 <youremail@domain.com>
2020-05-09 18:10:04 +0200

    Updates

---
+++ precache-manifest.d5b7fff747e6ed0401e809d332c556e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d98a669eed3cbcb31d3efc852a00809",
+    "url": "/TodoList-React/index.html"
+  },
+  {
+    "revision": "3b098dba97dd5411a6eb",
+    "url": "/TodoList-React/static/css/main.a9d576f9.chunk.css"
+  },
+  {
+    "revision": "6fb69d961aed97ff57c8",
+    "url": "/TodoList-React/static/js/2.59fe9dc2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList-React/static/js/2.59fe9dc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b098dba97dd5411a6eb",
+    "url": "/TodoList-React/static/js/main.2ad5e641.chunk.js"
+  },
+  {
+    "revision": "ee8b24830d979fc93e7d",
+    "url": "/TodoList-React/static/js/runtime-main.25b6fdf9.js"
+  }
+]);
